b3107fe24219d08ad4b859c694d472e0116f3361
abachrac <abachrac@d41ffffc-f2b2-4b44-b683-edcb61ded693>
2011-09-20 01:34:23 +0000

    add timestamp to messages

--- src/map_server/pixmap_server.cpp
+++ src/map_server/pixmap_server.cpp
@@ -1,6 +1,7 @@
 #include <lcm/lcm.h>
 #include <occ_map/PixelMap.hpp>
 #include <string>
+#include <sys/time.h>
 
 using namespace std;
 using namespace occ_map;
@@ -9,6 +10,13 @@ void usage(char * name)
 {
   fprintf(stderr, "usage: %s filename channel repeat_period(sec)\n", name);
   exit(1);
+}
+
+static int64_t _timestamp_now()
+{
+    struct timeval tv;
+    gettimeofday (&tv, NULL);
+    return (int64_t) tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
 int main(int argc, char ** argv)
@@ -28,6 +36,7 @@ int main(int argc, char ** argv)
   }
 
   occ_map_pixel_map_t * pix_map = PixelMap<float>::load_pixel_map_t_from_file(filename);
+  pix_map->utime = _timestamp_now();
 
   while (true) {
     occ_map_pixel_map_t_publish(lcm, channel, pix_map);
