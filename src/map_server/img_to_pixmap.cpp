d91f27c059ecd9ae73ad42d5ae5f7355ec2ebb73
abachrac <abachrac@d41ffffc-f2b2-4b44-b683-edcb61ded693>
2012-08-31 05:04:22 +0000

    add option to pass in the original pixmap to match sizing...

--- src/map_server/img_to_pixmap.cpp
+++ src/map_server/img_to_pixmap.cpp
@@ -14,17 +14,21 @@ static float uint8ToFloat(uint8_t v)
 
 void usage(char * name)
 {
-  fprintf(stderr, "usage: %s img_fname map_fname \n", name);
+  fprintf(stderr, "usage: %s img_fname pixmap_fname [orig_pixmap_fname]\n", name);
   exit(1);
 }
 
 int main(int argc, char ** argv)
 {
-  if (argc != 3)
+  if (argc < 3)
     usage(argv[0]);
 
   string img_fname = argv[1];
   string filename = argv[2];
+
+  string origMapFname;
+  if (argc > 3)
+    origMapFname = argv[3];
 
   //todo: get resolution and bounds from command line
   float resolution = 1;
@@ -42,7 +46,20 @@ int main(int argc, char ** argv)
   xy1[0] = xy0[0] + resolution * cvm->cols;
   xy1[1] = xy0[1] + resolution * cvm->rows;
 
-  Uint8PixelMap *u8map = new Uint8PixelMap(xy0, xy1, resolution);
+  if (!origMapFname.empty()) {
+    FloatPixelMap * orig_map = new FloatPixelMap(origMapFname);
+    for (int i = 0; i < 2; i++) {
+      xy0[i] = orig_map->xy0[i];
+      xy1[i] = orig_map->xy1[i];
+    }
+    resolution = orig_map->metersPerPixel;
+    int num_cells = cvm->cols * cvm->rows;
+    if (num_cells != orig_map->num_cells) {
+      fprintf(stderr, "ERROR: image has %d cells, but orig_map has %d\n", num_cells, orig_map->num_cells);
+    }
+  }
+
+  Uint8PixelMap *u8map = new Uint8PixelMap(xy0, xy1, resolution, 0, true, false);
   int ixy[2];
   for (ixy[0] = 0; ixy[0] < u8map->dimensions[0]; ixy[0]++) {
     for (ixy[1] = 0; ixy[1] < u8map->dimensions[1]; ixy[1]++) {
@@ -50,7 +67,7 @@ int main(int argc, char ** argv)
       u8map->writeValue(ixy, val);
     }
   }
-  FloatPixelMap * fmap = new FloatPixelMap(u8map,true, uint8ToFloat);
+  FloatPixelMap * fmap = new FloatPixelMap(u8map, true, uint8ToFloat);
   fmap->saveToFile(filename.c_str());
 
 }
